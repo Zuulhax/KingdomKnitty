2a9e594d0c021ed244d780f43b085fc89453a044
Zuulhax <hbreitenstein@mail.bradley.edu>
2025-04-17 16:43:48 -0500

    Third Iteration
    added a third level, more coins, raised walls, colored walls, more enemies, end scene, quit buttons on both the Main Menu and end scene, and doors that allow the player to go to the next level

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,31 @@
+/*****************************************************************************
+// File Name : DoorController.cs
+// Author : Hunter A. Breitenstein
+// Creation Date : April 17, 2025
+//
+// Brief Description : This script sends the player to the next scene in the build index when they touch the door
+*****************************************************************************/
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] private AudioSource doorEnter;
+
+
+    /// <summary>
+    /// loads the next scene in the build index upon colliding with the door(s)
+    /// </summary>
+    /// <param name="collidedObject"></param>
+    private void OnTriggerEnter(Collider collidedObject)
+    {
+        if (collidedObject.gameObject.name == "Player")
+        {
+            //load the next level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+            doorEnter.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/EndScript.cs
@@ -0,0 +1,25 @@
+/*****************************************************************************
+// File Name : EndScript.cs
+// Author : Hunter A. Breitenstein
+// Creation Date : April 17, 2025
+//
+// Brief Description : This script exits the game when the "Quit" button on screen is pressed
+*****************************************************************************/
+
+using UnityEngine;
+
+public class EndScript : MonoBehaviour
+{
+    /// <summary>
+    /// Exits the game when the button saying "Quit" is pressed
+    /// </summary>
+    public void EndGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+
+        if (Application.isPlaying)
+        {
+            Application.Quit();
+        }
+    }
+}
